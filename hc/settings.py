11a32cdc3921d5937d5dbafd7bd1f0f9bc1006ad
CharlesJonah <mutua.charles48@gmail.com>
2017-02-01 15:10:40 +0300

    Deployment version

--- hc/settings.py
+++ hc/settings.py
@@ -79,36 +79,50 @@ WSGI_APPLICATION = 'hc.wsgi.application'
 TEST_RUNNER = 'hc.api.tests.CustomRunner'
 
 
-# Default database engine is SQLite. So one can just check out code,
-# install requirements.txt and do manage.py runserver and it works
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':   './hc.sqlite',
-    }
-}
+DATABASES = {}
 
-# You can switch database engine to postgres or mysql using environment
-# variable 'DB'. Travis CI does this.
-if os.environ.get("DB") == "postgres":
-    DATABASES = {
-        'default': {
-            'ENGINE':   'django.db.backends.postgresql',
-            'NAME':     'hc',
-            'USER':     'postgres',
-            'TEST': {'CHARSET': 'UTF8'}
-        }
+if os.environ.get('TRAVIS_DB', None):
+    DATABASES['default'] = {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'hc',
+        'USER' : 'travis',
+        'PASSWORD' : '',
+        'HOST' : '127.0.0.1'
     }
+else:
+    DATABASES['default'] = dj_database_url.config()
 
-if os.environ.get("DB") == "mysql":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'USER':     'root',
-            'NAME':     'hc',
-            'TEST': {'CHARSET': 'UTF8'}
-        }
-    }
+#
+# # Default database engine is SQLite. So one can just check out code,
+# # install requirements.txt and do manage.py runserver and it works
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':   './hc.sqlite',
+#     }
+# }
+#
+# # You can switch database engine to postgres or mysql using environment
+# # variable 'DB'. Travis CI does this.
+# if os.environ.get("DB") == "postgres":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE':   'django.db.backends.postgresql',
+#             'NAME':     'hc',
+#             'USER':     'postgres',
+#             'TEST': {'CHARSET': 'UTF8'}
+#         }
+#     }
+#
+# if os.environ.get("DB") == "mysql":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'USER':     'root',
+#             'NAME':     'hc',
+#             'TEST': {'CHARSET': 'UTF8'}
+#         }
+#     }
 
 LANGUAGE_CODE = 'en-us'
 
